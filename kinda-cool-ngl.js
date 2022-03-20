289e29b3a5396290345b82e7063e8e661a19bf1b
ImranBary <100944041+ImranBary@users.noreply.github.com>
2022-03-20 09:39:53 +0000

    No more messing about for the weekend

---
+++ kinda-cool-ngl.js
@@ -0,0 +1,20 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+.to('.mars-2',750,{y:-300})
+.to('.starman',750,{y:-200},'-=750')
+.fromTo('.mars-1',750,{y:-50},{y:0,duration:750 },'-=750')
+.to('.content',750,{top:'0%'},'-=750')
+.fromTo('.text',{opacity:0},{opacity:1,duration:750});
+
+
+let scene = new ScrollMagic.Scene({
+    triggerElement:"section",
+    duration:"80%",
+    triggerHook:0,
+})
+
+.setTween(timeline)
+.setPin("section")
+.addTo(controller);
